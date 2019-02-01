30537775199e38da43277019d87f5ff83f5bd2d8
battlecook <com2us>
2019-02-01 20:58:25 +0900

    change architecture  ( in progress )
    add dirty check in LeafNode update function

--- src/DataStructure/LeafNode.php
+++ src/DataStructure/LeafNode.php
@@ -5,13 +5,21 @@ namespace battlecook\DataStructure;
 
 final class LeafNode extends Node
 {
+    const NONE = 0;
+    const ADDED = 1;
+    const UPDATED = 2;
+    const DELETED = 4;
+
     private $data;
+    private $keys;
     private $status;
 
-    public function __construct($pointer, $data)
+    public function __construct($pointer, array $data, array $keys)
     {
         parent::__construct($pointer);
+
         $this->data = $data;
+        $this->keys = $keys;
     }
 
     public function getStatus(): int
@@ -19,8 +27,30 @@ final class LeafNode extends Node
         return $this->status;
     }
 
-    public function update($data)
+    public function update(array $data): bool
     {
+        if($this->status === self::DELETED)
+        {
+            return false;
+        }
 
+        $isChanged = false;
+        foreach($this->keys as $key)
+        {
+            if($this->data[$key] === $data[$key])
+            {
+                continue;
+            }
+            $this->data[$key] = $data[$key];
+            if($isChanged === false)
+            {
+                $isChanged = true;
+            }
+        }
+
+        if($isChanged === true && $this->status !== self::UPDATED)
+        {
+            $this->status = self::UPDATED;
+        }
     }
 }
